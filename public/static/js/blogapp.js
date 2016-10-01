3515cb99cf4d9903d189a72955cf4c889d617d79
Sam Bartrum <sambartrum@googlemail.com>
2016-10-01 20:07:39 +0100

    Changed angular interpolation symbols to make things easier to read/distinguish

--- public/static/js/blogapp.js
+++ public/static/js/blogapp.js
@@ -1,5 +1,10 @@
 var blogmodule = angular.module('blogmodule', []);
 
+blogmodule.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+});
+
 blogmodule.controller('TestController', ['$scope', function($scope) {
     console.log('hi');
     $scope.testMessage = "WHAT'S UP MARK";
