f157e9b220e275de4f92d01defdaa3e6c966ee2c
Sam Bartrum <sambartrum@googlemail.com>
2016-10-08 23:46:05 +0100

    Tweaks

--- routes.js
+++ routes.js
@@ -2,8 +2,27 @@ var BlogPost = require('./models/blogpost')
 
 module.exports = function(app){
 
+//    Main entrance page - has snippets of all the previous blog posts on
+    app.get('/', function(req, res){
+        BlogPost.find({}).sort([['date', -1]]).exec(function (err, posts){
+            res.render(__dirname + '/public/views/blogposts', {params: {posts: posts}});
+        })
+    });
+
+
+
     app.get('/admin', function(req, res){
         res.render(__dirname + '/public/views/admin', {params: {sam: 'I am sam'}});
+    });
+
+    app.get('/post/:id', function(req, res){
+        BlogPost.findOne({_id: req.params.id}).exec(function (err, post){
+            res.render(__dirname + '/public/views/blogpost', {params: {post: post}});
+        })
+    });
+
+    app.get('/newpost', function(req,res){
+         res.render(__dirname + '/public/views/newpost');
     });
 
     app.post('/savepost', function(req, res){
@@ -11,13 +30,11 @@ module.exports = function(app){
         req.body.date = today
         var newPost = new BlogPost(req.body)
         newPost.save()
-        res.redirect('/posts')
     });
 
-    app.get('/posts', function(req, res){
-        BlogPost.find({}).sort([['date', -1]]).exec(function (err, posts){
-            res.render(__dirname + '/public/views/index', {params: {posts: posts}});
-        })
+    app.post('/updatepost/:id', function(req, res){
+        BlogPost.update({_id: req.param.id},
+                        {blogtitle: req.params.blogtitle, blogpost: req.params.blogpost})
     });
 
     app.get('/deletepost/:id', function(req, res){
